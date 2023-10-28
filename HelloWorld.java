35f30d800ceddcd7fd0684ee1a75fd52dc531985
LucaGiordano214 <lgiordano@cerebotani.it>
2023-10-28 08:14:11 +0200

    FunzionalitàSomma

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 class HelloWorld{
 	public static void main(String args[]){
-		System.out.println("Hello World!");
+		int num1 = 5;
+		int num2 = 3;
+		System.out.println(num1 + " + " + num2 + " = " + (num1+num2));
 	}
 }
